a0ac46e9d581ef55400e511bae1bf532f1af04b9
Marius Rackwitz <git@mariusrackwitz.de>
2015-05-08 19:10:45 +0200

    Deduplicate pod targets

--- spec/unit/installer/analyzer_spec.rb
+++ spec/unit/installer/analyzer_spec.rb
@@ -13,6 +13,11 @@ module Pod
           pod 'AFNetworking',                '1.0.1'
           pod 'SVPullToRefresh',             '0.4'
           pod 'libextobjc/EXTKeyPathCoding', '0.2.3'
+
+          target 'TestRunner' do
+            pod 'libextobjc/EXTKeyPathCoding', '0.2.3'
+            pod 'libextobjc/EXTSynthesize',    '0.2.3'
+          end
         end
 
         hash = {}
@@ -44,7 +49,7 @@ module Pod
 
       it 'computes the state of the Podfile respect to the Lockfile' do
         state = @analyzer.analyze.podfile_state
-        state.added.should     == %w(AFNetworking libextobjc/EXTKeyPathCoding)
+        state.added.should     == %w(AFNetworking libextobjc/EXTKeyPathCoding libextobjc/EXTSynthesize)
         state.changed.should   == %w()
         state.unchanged.should == %w(JSONKit SVPullToRefresh)
         state.deleted.should   == %w(NUI)
@@ -102,9 +107,9 @@ module Pod
       it 'generates the libraries which represent the target definitions' do
         target = @analyzer.analyze.targets.first
         target.pod_targets.map(&:name).sort.should == [
-          'Pods-JSONKit',
-          'Pods-AFNetworking',
-          'Pods-SVPullToRefresh',
+          'JSONKit',
+          'AFNetworking',
+          'SVPullToRefresh',
           'Pods-libextobjc',
         ].sort
         target.support_files_dir.should == config.sandbox.target_support_files_dir('Pods')
@@ -268,6 +273,7 @@ module Pod
           'JSONKit (1.5pre)',
           'SVPullToRefresh (0.4)',
           'libextobjc/EXTKeyPathCoding (0.2.3)',
+          'libextobjc/EXTSynthesize (0.2.3)',
         ]
       end
 
